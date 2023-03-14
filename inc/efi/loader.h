241ddaa8d2c6df244976326ed947400386137761
devos_c <loukmane.khaled@gmail.com>
2023-03-14 21:46:30 +0100

    ,

--- inc/efi/loader.h
+++ inc/efi/loader.h
@@ -2,3 +2,20 @@
  * Bootloader defines
 */
 
+#pragma once
+#include <Uefi.h>
+#include <Library/UefiLib.h>
+
+typedef struct _FRAME_BUFFER_DESCRIPTOR{
+	UINT32 		HorizontalResolution;
+	UINT32 		VerticalResolution;
+	UINT64      Pitch;
+	UINT32* 	BaseAddress; // for G.O.P
+	UINT64 	    FbSize;
+} FRAME_BUFFER_DESCRIPTOR;
+
+typedef struct _NOS_INITDATA {
+    FRAME_BUFFER_DESCRIPTOR FrameBuffer;
+    EFI_MEMORY_DESCRIPTOR* MemoryMap;
+
+} NOS_INITDATA;
---
+++ src/nos/init.c
@@ -0,0 +1,13 @@
+/*
+ * init.c
+ * This file contains the initialization function of the Operating System Kernel
+*/
+
+#include <nos.h>
+
+/*
+ * The initialization entry point of the NOS Kernel
+*/
+void NOSAPI __declspec(noreturn) NosInit() {
+
+}
