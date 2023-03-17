01b82c7ed950e7d0e6c9d4f5c51d73c95ea2ee02
devos_c <loukmane.khaled@gmail.com>
2023-03-17 21:09:39 +0100

    efi boot

--- inc/efi/loader.h
+++ inc/efi/loader.h
@@ -5,6 +5,9 @@
 #pragma once
 #include <Uefi.h>
 
+#define PE_SECTION_CODE 0x20
+#define PE_SECTION_INITIALIZED_DATA 0x40
+#define PE_SECTION_UNINITIALIZED_DATA 0x80
 
 typedef struct _FRAME_BUFFER_DESCRIPTOR{
 	UINT32 		HorizontalResolution;
@@ -20,9 +23,161 @@ typedef struct _NOS_INITDATA {
 
 } NOS_INITDATA;
 
+
+typedef struct _IMAGE_IMPORT_ADDRESS_TABLE {
+    UINT64 ImportAddress;
+    UINT32 ForwarderRva;
+} IMAGE_IMPORT_ADDRESS_TABLE, *PIMAGE_IMPORT_ADDRESS_TABLE;
+
+// We support only PE32+ 64 bit image files
+typedef struct {
+    UINT16 magic; // must be 0x20b
+    UINT8 MajorLinkerVersion;
+    UINT8 MinorLinkerVersion;
+    UINT32 SizeofCode;
+    UINT32 SizeofInitializedData;
+    UINT32 SizeofUninitializedData; // .bss ...
+    UINT32 EntryPointAddr;
+    UINT32 BaseOfCode;
+} PE_OPTIONAL_HEADER;
+
+typedef struct {
+    UINT64 ImageBase;
+    UINT32 SectionAlignment; // greater than or equal to file alignement
+    UINT32 FileAlignment;
+    UINT16 MajorOsVersion;
+    UINT16 MinorOsVersion;
+    UINT16 MajorImageVersion;
+    UINT16 MinorImageVersion;
+    UINT16 MajorSubsystemVersion;
+    UINT16 MinorSubsystemVersion;
+    UINT32 Win32VersionValue; // reserved must be 0
+    UINT32 SizeofImage;
+    UINT32 SizeofHeaders;
+    UINT32 CheckSum;
+    UINT16 Subsystem; // this is used to load image ex. from main or wWinMain or driver entry ...
+    UINT16 DllCharacteristics;
+    UINT64 StackReserve;
+    UINT64 StackCommit;
+    UINT64 HeapReserve;
+    UINT64 HeapCommit;
+    UINT32 LoaderFlags; // reserved must be 0
+    UINT32 NumDataDirectories;
+} PE_WINDOWS_SPECIFIC_FIELDS;
+typedef struct {
+    char name[8];
+    UINT32 VirtualSize;
+    UINT32 VirtualAddress;
+    UINT32 SizeofRawData; // if less than virtual size reminder of file align is filled with 0
+    UINT32 PtrToRawData; // for object file it should be 4 byte aligned for best performance
+    UINT32 PtrToRelocations; // should be 0 for executables
+    UINT32 PtrToLineNumbers;
+    UINT16 NumRelocations; // set to 0 for executables
+    UINT16 NumLineNumbers;
+    UINT32 Characteristics; // section flags
+} PE_SECTION_TABLE;
+
+typedef struct _IMAGE_DATA_DIRECTORY {
+    UINT32   VirtualAddress;
+    UINT32   Size;
+} IMAGE_DATA_DIRECTORY, * PIMAGE_DATA_DIRECTORY;
+
+typedef struct _OPTIONNAL_HEADER_DATA_DIRECTORIES {
+    IMAGE_DATA_DIRECTORY ExportTable;
+    IMAGE_DATA_DIRECTORY ImportTable;
+    IMAGE_DATA_DIRECTORY ResourceTable;
+    IMAGE_DATA_DIRECTORY ExceptionTable;
+    IMAGE_DATA_DIRECTORY CertificateTable;
+    IMAGE_DATA_DIRECTORY BaseRelocationTable;
+    IMAGE_DATA_DIRECTORY Debug;
+    IMAGE_DATA_DIRECTORY Architecture;
+    IMAGE_DATA_DIRECTORY GlobalPtr;
+    IMAGE_DATA_DIRECTORY ThreadLocalStorageTable;
+    IMAGE_DATA_DIRECTORY LoadConfigTable;
+    IMAGE_DATA_DIRECTORY BoundImport;
+    IMAGE_DATA_DIRECTORY ImportAddressTable;
+    IMAGE_DATA_DIRECTORY DelayImportDescriptor;
+    IMAGE_DATA_DIRECTORY ClrRuntimeHeader;
+    IMAGE_DATA_DIRECTORY Reserved;
+} OPTIONNAL_HEADER_DATA_DIRECTORIES;
+
+typedef struct {
+    char Signature[4]; // PE\0\0
+    UINT16 MachineType; // must be AMD64, for x86_64 and AMD64
+    UINT16 NumSections;
+    UINT32 TimeDateStamp;
+    UINT32 SymbolTableOffset;
+    UINT32 NumSymbols;
+    UINT16 SizeofOptionnalHeader;
+    UINT16 Characteristics;
+    PE_OPTIONAL_HEADER OptionnalHeader; // not optional because it needs to be a loadable image
+    PE_WINDOWS_SPECIFIC_FIELDS ThirdHeader;
+    OPTIONNAL_HEADER_DATA_DIRECTORIES OptionnalDataDirectories;
+} PE_IMAGE_HDR;
+
+
+
+typedef struct _IMAGE_IMPORT_DIRECTORY {
+    UINT32 ImportLookupTable;
+    UINT32 TimeDataStamp;
+    UINT32 ForwarderChain;
+    UINT32 NameRva;
+    UINT32 ImportAddressTableRva;
+} IMAGE_IMPORT_DIRECTORY, *PIMAGE_IMPORT_DIRECTORY;
+
+enum IMAGE_BASE_RELOCATION_TYPE{
+    IMAGE_REL_BASED_ABSOLUTE = 0,
+    IMAGE_REL_BASED_HIGH = 1,
+    IMAGE_REL_BASED_LOW = 2,
+    IMAGE_REL_BASED_HIGHLOW = 3,
+    IMAGE_REL_BASED_HIGHADJ = 4,
+    IMAGE_REL_BASED_MIPS_JMPADDR = 5,
+    IMAGE_REL_BASED_DIR64 = 10
+};
+
+typedef struct _IMAGE_BASE_RELOCATION_ENTRY {
+    UINT16 Offset : 12; // 4 bits Type, 12 bits Offset
+    UINT16 Type : 4;
+} IMAGE_BASE_RELOCATION_ENTRY, * PIMAGE_BASE_RELOCATION_ENTRY;
+
+typedef struct _IMAGE_BASE_RELOCATION_BLOCK {
+    UINT32 PageRva; // Page Relative Virtual Address
+    UINT32 BlockSize; // total num bytes in base relocation block
+    IMAGE_BASE_RELOCATION_ENTRY RelocationEntries[];
+} IMAGE_BASE_RELOCATION_BLOCK, *PIMAGE_BASE_RELOCATION_BLOCK;
+
+typedef struct _IMAGE_EXPORT_DIRECTORY {
+    UINT32 ExportFlags; // reserved must be 0
+    UINT32 TimeDateStamp;
+    UINT16 MajorVersion;
+    UINT16 MinorVersion;
+    UINT32 NameRva; // Dll Name
+    UINT32 OrdinalBase;
+    UINT32 NumAddressTableEntries;
+    UINT32 NumNamePointers; // = NumOrdinalEntries
+    UINT32 ExportAddressTableRva;
+    UINT32 NamePointerRva;
+    UINT32 OrdinalTableRva;
+} IMAGE_EXPORT_DIRECTORY, *PIMAGE_EXPORT_DIRECTORY;
+
+typedef struct _IMAGE_EXPORT_ADDRESS_TABLE {
+    UINT32 ExportRva;
+    UINT32 ForwarderRva; // this one gives the dllname.exportname or export name with ordinal number
+} IMAGE_EXPORT_ADDRESS_TABLE, *PIMAGE_EXPORT_ADDRESS_TABLE;
+
+typedef struct _IMAGE_HINT_NAME_TABLE {
+    UINT16 Hint;
+    char Name[];
+} IMAGE_HINT_NAME_TABLE, *PIMAGE_HINT_NAME_TABLE;
+
+#define IMAGE_ORDINAL_IMPORT_FLAG 0x8000000000000000
+#define IMAGE_ORDINAL_NUMBER(LookupEntry) (LookupEntry & 0xffff)
+#define IMAGE_HINT_NAME_RVA(LookupEntry) (LookupEntry & (0x7fffffff));
+
 #define MBR_SIGNATURE 0xAA55
 #define GPT_SIGNATURE "EFI PART "
 #define FSID_GPT 0xEE
+
 #pragma pack(push, 1)
 
 typedef struct _MASTER_BOOT_RECORD{
--- src/boot/efi/amd64/boot.c
+++ src/boot/efi/amd64/boot.c
@@ -4,6 +4,7 @@
 #include <Protocol/LoadedImage.h>
 #include <Protocol/BlockIo.h>
 #include <Protocol/SimpleFileSystem.h>
+#include <Guid/FileInfo.h>
 #include "../../../../inc/efi/loader.h"
 
 NOS_INITDATA NosInitData = {0};
@@ -24,10 +25,9 @@ GUID_PARTITION_TABLE_HEADER GptHeader;
 GUID_PARTITION_ENTRY* GptEntries;
 
 
-
 /*
- * BlInitBootGraphics
- * This function sets the Graphics Output to Native Mode and claims display information
+ * BlNullGuid
+ * Checks if the Guid is NULL ({0, 0, 0, 0})
 */
 
 static inline BOOLEAN BlNullGuid(EFI_GUID Guid) {
@@ -37,6 +37,37 @@ static inline BOOLEAN BlNullGuid(EFI_GUID Guid) {
 	return FALSE;
 }
 
+/*
+ * isMemEqual
+ * Checks if the memory at "a" contains the same value as the memory at "b"
+*/
+
+BOOLEAN isMemEqual(void* a, void* b, UINT64 Count) {
+	for(UINT64 i = 0;i<Count;i++) {
+		if(((char*)a)[i] != ((char*)b)[i]) return FALSE;
+	}
+	return TRUE;
+}
+
+
+void CopyAlignedMemory(void* _dest, void* _src, UINT64 NumBytes) {
+	NumBytes >>= 3;
+	for(UINT64 i = 0;i<NumBytes;i++) {
+		((UINT64*)_dest)[i] = ((UINT64*)_src)[i];
+	}
+}
+void ZeroAlignedMemory(void* _dest, UINT64 NumBytes) {
+	NumBytes >>= 3;
+	for(UINT64 i = 0;i<NumBytes;i++) {
+		((UINT64*)_dest)[i] = 0;
+	}
+}
+
+
+/*
+ * BlInitBootGraphics
+ * This function sets the Graphics Output to Native Mode and claims display information
+*/
 void BlInitBootGraphics() {
 	EFI_GRAPHICS_OUTPUT_PROTOCOL* GraphicsProtocol;
 	// Check for G.O.P Support
@@ -62,6 +93,74 @@ void BlInitBootGraphics() {
 	NosInitData.FrameBuffer.Pitch = ModeInfo->PixelsPerScanLine;
 }
 
+int BlGetPeHeaderOffset(void* hdr) {
+	if (!isMemEqual(hdr, "MZ", 2)) return 0;
+	return *(int*)((char*)hdr + 0x3c);
+}
+
+BOOLEAN BlCheckImageHeader(PE_IMAGE_HDR* hdr) {
+	if (
+		hdr->ThirdHeader.Subsystem != 1 ||
+		hdr->MachineType != 0x8664 ||
+		hdr->SizeofOptionnalHeader < sizeof(PE_OPTIONAL_HEADER) ||
+		!hdr->OptionnalHeader.EntryPointAddr ||
+		!isMemEqual(hdr->Signature, "PE\0\0", 4)
+		) {
+		return FALSE;
+	}
+	return TRUE;
+}
+
+
+
+BOOLEAN BlLoadImage(void* Buffer, PE_IMAGE_HDR** HdrStart, void** VirtualAddressSpace, UINT64* VasSize) {
+	PE_IMAGE_HDR* Header;
+	UINT64 VasBufferSize = 0;
+	void* VasBuffer;
+	EFI_PHYSICAL_ADDRESS ImageBase;
+	// Header Check
+	{
+		int Off = BlGetPeHeaderOffset(Buffer);
+		if(!Off) return FALSE;
+		Header = (PE_IMAGE_HDR*)((char*)Buffer + Off);
+		if(!BlCheckImageHeader(Header)) return FALSE;
+		*HdrStart = Header;
+	}
+	// Get virtual address buffer size
+	PE_SECTION_TABLE* Sections = (PE_SECTION_TABLE*)((char*)Header + Header->SizeofOptionnalHeader);
+	for(int i = 0;i<Header->NumSections;i++) {
+		if(Sections[i].Characteristics & ((PE_SECTION_CODE | PE_SECTION_INITIALIZED_DATA | PE_SECTION_UNINITIALIZED_DATA))) {
+			if(Sections[i].VirtualSize < Sections[i].SizeofRawData) Sections[i].VirtualSize = Sections[i].SizeofRawData;
+			if(Sections[i].VirtualAddress + Sections[i].VirtualSize > VasBufferSize) VasBufferSize = Sections[i].VirtualAddress + Sections[i].VirtualSize;
+		}
+	}
+	// Allocate virtual address buffer
+	VasBufferSize += 0x10000;
+
+	if(EFI_ERROR(gBS->AllocatePages(AllocateAnyPages, EfiLoaderData, VasBufferSize >> 12, &ImageBase))) {
+		Print(L"Failed to allocate memory\n");
+		gBS->Exit(gImageHandle, EFI_UNSUPPORTED, 0, NULL);
+	}
+	if(Header->ThirdHeader.DllCharacteristics & 0x40) {
+		// TODO : Relocation
+	}
+
+	VasBuffer = (void*)ImageBase;
+	// Copy section data to the VAS Buffer
+	for(int i = 0;i<Header->NumSections;i++) {
+		PE_SECTION_TABLE* Section = Sections + i;
+		if (Section->Characteristics & (PE_SECTION_CODE | PE_SECTION_INITIALIZED_DATA | PE_SECTION_UNINITIALIZED_DATA)) {
+			CopyAlignedMemory((void*)((char*)VasBuffer + Section->VirtualAddress), (UINT64*)((char*)Buffer + Section->PtrToRawData), Section->SizeofRawData);
+			if (Section->VirtualSize > Section->SizeofRawData) {
+				UINT64 UninitializedDataSize = Section->VirtualSize - Section->SizeofRawData;
+				ZeroAlignedMemory((void*)((char*)VasBuffer + Section->VirtualAddress + Section->SizeofRawData), UninitializedDataSize);
+			}
+		}
+	}
+	*VirtualAddressSpace = VasBuffer;
+	*VasSize = VasBufferSize;
+	return TRUE;
+}
 
 /*
  * NOS AMD64 UEFI Bootloader Entry Point
@@ -139,13 +238,72 @@ EFI_STATUS EFIAPI UefiEntry(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* Syst
 	}
 
 	// Load the kernel (TODO : Load Modules and drivers)
-
+	EFI_FILE* Kernel;
+	if(EFI_ERROR(OsPartition->Open(OsPartition, &Kernel, L"NewOS\\System\\noskx64.exe", EFI_FILE_MODE_READ, 0))) {
+		Print(L"noskx64.exe is missing.\n");
+		return EFI_NOT_FOUND;
+	}
+	char __finf[sizeof(EFI_FILE_INFO) + 0x100]; // Path to kernel
+	EFI_FILE_INFO* FileInfo = (EFI_FILE_INFO*)__finf;
 	
+	UINTN BufferSize = sizeof(EFI_FILE_INFO) + 0x100;
+	
+	if(EFI_ERROR(Kernel->GetInfo(Kernel, &gEfiFileInfoGuid, &BufferSize, (void*)FileInfo))) {
+		Print(L"failed to get info\n");
+		while(1);
+	}
+	BufferSize = FileInfo->FileSize;
+	void* KernelBuffer;
+	
+	if(EFI_ERROR(gBS->AllocatePool(EfiLoaderData, BufferSize, &KernelBuffer))) {
+		Print(L"Failed to allocate memory\n");
+		return EFI_UNSUPPORTED;
+	}
+	Kernel->Read(Kernel, &BufferSize, KernelBuffer);
+	PE_IMAGE_HDR* ImageHeader;
+	void* Vas;
+	UINT64 VasSize;
+	if(!BlLoadImage(KernelBuffer, &ImageHeader, &Vas, &VasSize)) {
+		Print(L"Failed to load noskx64.exe, File maybe corrupt.\n");
+		return EFI_UNSUPPORTED;
+	}
+	
+	Print(L"StartAddr : 0x%0X , Base : 0x%llu\n", ImageHeader->OptionnalHeader.EntryPointAddr, ImageHeader->ThirdHeader.ImageBase);
+
+	// Get the memory map
+	UINTN MapSize = 0, DescriptorSize = 0, MapKey = 0;
+	UINT32 DescriptorVersion = 0;
+	EFI_MEMORY_DESCRIPTOR* MemoryMap = NULL;
+	EFI_STATUS Status = gBS->GetMemoryMap(&MapSize, MemoryMap, &MapKey, &DescriptorSize, &DescriptorVersion);
+	if(Status != EFI_BUFFER_TOO_SMALL) {
+		Print(L"Failed to get memory map\n");
+		return EFI_UNSUPPORTED;
+	}
+	MapSize += 3 * DescriptorSize;
+	gBS->AllocatePool(EfiLoaderData, MapSize, (void**)&MemoryMap);
+	if(EFI_ERROR(gBS->GetMemoryMap(&MapSize, MemoryMap, &MapKey, &DescriptorSize, &DescriptorVersion))) {
+		Print(L"Failed to get memory map\n");
+		return EFI_UNSUPPORTED;
+	}
 
 	// Disable Watchdog Timer
 	gBS->SetWatchdogTimer(0, 0, 0, NULL);
 
-	Print(L"Success.");
+	// Exit boot services
+	if(EFI_ERROR(gBS->ExitBootServices(ImageHandle, MapKey))) {
+		Print(L"Failed to exit boot services\n");
+		return EFI_UNSUPPORTED;
+	}
+	if(EFI_ERROR(gST->RuntimeServices->SetVirtualAddressMap(MapSize, DescriptorSize, DescriptorVersion, MemoryMap))) {
+		for(UINTN i = 0;i<0x1000;i++) {
+		((UINT32*)NosInitData.FrameBuffer.BaseAddress)[i] = 0xFF00;
+		while(1);
+	}
+	}
+
+	for(UINTN i = 0;i<0x1000;i++) {
+		((UINT32*)NosInitData.FrameBuffer.BaseAddress)[i] = 0xFF;
+	}
 	while(1);
 	return EFI_SUCCESS;
 }
