1af6b752304e6d541b2f75476725f1e069265867
devos_c <loukmane.khaled@gmail.com>
2023-05-31 13:18:53 +0100

    remove comments

--- src/nos/ob/handle.c
+++ src/nos/ob/handle.c
@@ -1,21 +1,5 @@
 #include <nos/ob/obutil.h>
 
-/*
-Allocates an object in the object table
-This mechanism is used to make it easier for the Ob Subsystem to find an object by its addresss
-*/
-
-/*
-The Object manager uses the quarter of the top of the address space
-*/
-
-/*
-Each ob map contains 63 objects and 512 bytes of space
-*/
-/*
-It is headed by a full key
-*/
-
 
 HANDLE ObOpenObjectByName(
     PEPROCESS Process,
